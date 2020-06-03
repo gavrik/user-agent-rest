4d37f733cdd5e4ca3702357fa5a8a48852c343bc
Mike Gavrylov <gavrikster@gmail.com>
2020-06-03 14:29:12 +0300

    redesign API package

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
-package useragentrest
+package main
 
 import (
-	"io/ioutil"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"github.com/ua-parser/uap-go/uaparser"
+	ua "useragentrest"
 )
 
-// UaParser - Return parsed UA string in JSON format
-func UaParser(c *gin.Context) {
-	parser := uaparser.NewFromSaved()
-	x, _ := ioutil.ReadAll(c.Request.Body)
-	client := parser.Parse(string(x))
-	c.JSON(http.StatusOK, client)
-}
-
-// SetupRouter - setup router Handlers
-func SetupRouter() *gin.Engine {
-	route := gin.Default()
-	route.POST("/ua", UaParser)
-	return route
-}
-
 func main() {
-	router := SetupRouter()
+	router := ua.SetupRouter()
 	router.Run()
 }
